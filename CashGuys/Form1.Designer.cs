af296a7145d65534ab36f1ed989f6cc426f286f2
Kim Olsen <krimmero@hotmail.com>
2016-08-26 15:28:58 +0200

    label2 name changed to bobCashLabel

--- CashGuys/Form1.Designer.cs
+++ CashGuys/Form1.Designer.cs
@@ -29,45 +29,49 @@
         private void InitializeComponent()
         {
             this.joeCashLabel = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.bobCashLabel = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // joeCashLabel
             // 
             this.joeCashLabel.AutoSize = true;
-            this.joeCashLabel.Location = new System.Drawing.Point(13, 13);
+            this.joeCashLabel.Location = new System.Drawing.Point(17, 16);
+            this.joeCashLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.joeCashLabel.Name = "joeCashLabel";
-            this.joeCashLabel.Size = new System.Drawing.Size(35, 13);
+            this.joeCashLabel.Size = new System.Drawing.Size(46, 17);
             this.joeCashLabel.TabIndex = 0;
             this.joeCashLabel.Text = "label1";
             // 
-            // label2
+            // bobCashLabel
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 30);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.bobCashLabel.AutoSize = true;
+            this.bobCashLabel.Location = new System.Drawing.Point(17, 37);
+            this.bobCashLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.bobCashLabel.Name = "bobCashLabel";
+            this.bobCashLabel.Size = new System.Drawing.Size(46, 17);
+            this.bobCashLabel.TabIndex = 1;
+            this.bobCashLabel.Text = "label2";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 47);
+            this.label3.Location = new System.Drawing.Point(17, 58);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(46, 17);
             this.label3.TabIndex = 2;
             this.label3.Text = "label3";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(379, 321);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.bobCashLabel);
             this.Controls.Add(this.joeCashLabel);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -78,7 +82,7 @@
         #endregion
 
         private System.Windows.Forms.Label joeCashLabel;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label bobCashLabel;
         private System.Windows.Forms.Label label3;
     }
 }
