6fb65c666170c4ca77968d0b3afc6bee90fbcfef
Kim <Kim@DESKTOP-0ST89J1>
2016-08-26 16:28:18 +0200

    Guy class created

--- CashGuys/Form1.Designer.cs
+++ CashGuys/Form1.Designer.cs
@@ -30,48 +30,66 @@
         {
             this.joeCashLabel = new System.Windows.Forms.Label();
             this.bobCashLabel = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.bankCashLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // joeCashLabel
             // 
             this.joeCashLabel.AutoSize = true;
-            this.joeCashLabel.Location = new System.Drawing.Point(17, 16);
-            this.joeCashLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.joeCashLabel.Location = new System.Drawing.Point(13, 13);
             this.joeCashLabel.Name = "joeCashLabel";
-            this.joeCashLabel.Size = new System.Drawing.Size(46, 17);
+            this.joeCashLabel.Size = new System.Drawing.Size(35, 13);
             this.joeCashLabel.TabIndex = 0;
             this.joeCashLabel.Text = "label1";
             // 
             // bobCashLabel
             // 
             this.bobCashLabel.AutoSize = true;
-            this.bobCashLabel.Location = new System.Drawing.Point(17, 37);
-            this.bobCashLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.bobCashLabel.Location = new System.Drawing.Point(13, 30);
             this.bobCashLabel.Name = "bobCashLabel";
-            this.bobCashLabel.Size = new System.Drawing.Size(46, 17);
+            this.bobCashLabel.Size = new System.Drawing.Size(35, 13);
             this.bobCashLabel.TabIndex = 1;
             this.bobCashLabel.Text = "label2";
             // 
-            // label3
+            // bankCashLabel
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(17, 58);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(46, 17);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "label3";
+            this.bankCashLabel.AutoSize = true;
+            this.bankCashLabel.Location = new System.Drawing.Point(13, 47);
+            this.bankCashLabel.Name = "bankCashLabel";
+            this.bankCashLabel.Size = new System.Drawing.Size(35, 13);
+            this.bankCashLabel.TabIndex = 2;
+            this.bankCashLabel.Text = "label3";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 112);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 56);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Give $10 to Joe";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(160, 112);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 56);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Receive $5 from Bob";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(379, 321);
-            this.Controls.Add(this.label3);
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.bankCashLabel);
             this.Controls.Add(this.bobCashLabel);
             this.Controls.Add(this.joeCashLabel);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -83,7 +101,9 @@
 
         private System.Windows.Forms.Label joeCashLabel;
         private System.Windows.Forms.Label bobCashLabel;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label bankCashLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
---
+++ CashGuys/Guy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CashGuys
+{
+    class Guy
+    {
+        public string name;
+        public int cash;
+
+        public int GiveCash(int amount)
+        {
+            if (amount <= cash && amount > 0)
+            {
+                cash -= amount;
+                return amount;
+            }
+            else
+            {
+                MessageBox.Show("I dont have enough cash to give you " + amount, name + " says...");
+                return 0;
+            }
+        }
+
+        public int ReceiveCash(int amount)
+        {
+            if (amount > 0)
+            {
+                cash += amount;
+                return amount;
+            }
+            else
+            {
+                MessageBox.Show(amount + " isn't an amount i'll take", name + "says...");
+                return 0;
+            }
+        }
+    }
+}
